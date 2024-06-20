56e0f6f5b21d897fed75ebfe4897c35a07321cdf
cannedBear <xixiseattle@icloud.com>
2024-06-20 00:12:57 -0400

    Initial commit with cleaned history

---
+++ lib/pages/student_line/student_line_model.dart
@@ -0,0 +1,37 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import 'student_line_widget.dart' show StudentLineWidget;
+import 'package:flutter/material.dart';
+
+class StudentLineModel extends FlutterFlowModel<StudentLineWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  final unfocusNode = FocusNode();
+  // State field(s) for MouseRegion widget.
+  bool mouseRegionHovered1 = false;
+  // State field(s) for MouseRegion widget.
+  bool mouseRegionHovered2 = false;
+  // State field(s) for PageView widget.
+  PageController? pageViewController1;
+
+  int get pageViewCurrentIndex1 => pageViewController1 != null &&
+          pageViewController1!.hasClients &&
+          pageViewController1!.page != null
+      ? pageViewController1!.page!.round()
+      : 0;
+  // State field(s) for PageView widget.
+  PageController? pageViewController2;
+
+  int get pageViewCurrentIndex2 => pageViewController2 != null &&
+          pageViewController2!.hasClients &&
+          pageViewController2!.page != null
+      ? pageViewController2!.page!.round()
+      : 0;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    unfocusNode.dispose();
+  }
+}
