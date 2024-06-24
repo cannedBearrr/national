d2cdbcff970d9cec7aeccc4c3a08983a61bf15f8
cannedBear <xixiseattle@icloud.com>
2024-06-24 11:48:39 -0400

    fixed minor issues

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,6 +12,6 @@ import 'package:website_coding2024_nationals/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp(apiKey: '',));
+    await tester.pumpWidget(const MyApp(apiKey: '',));
   });
 }
